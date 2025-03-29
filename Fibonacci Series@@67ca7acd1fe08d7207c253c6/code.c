9d4b43ed398a9e8534bef3a8f195709d5302a449
Manish <manishchurnia@gmail.com>
2025-03-29 11:21:58 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fiboncci(int n){
+    if(n==0){
+        return 0;
+    }
+    else if(n==1){
+        return 1;
+    }
+    return fiboncci(n-1) + fiboncci(n-2);
+}
+
+int main(){
+    int a;
+    scanf("%d", &a);
+    for(int i=0; i<a; i++){
+        printf("%d ", fiboncci(a));
+    }
+    return 0;
+}
